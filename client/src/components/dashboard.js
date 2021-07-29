4988bc69a2f649f33335e50991464282a5e22950
Haripriya <haripriyagoswami1@gmail.com>
2021-07-29 19:10:22 +0530

    v1.0

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
 	return (
 		<div className="tile">
 			<div className="tile"></div>
 			<div
-				className="tile is-3"
+				className="tile section is-4"
 				style={{
 					backgroundColor: "#eee",
 					height: "100vh",
-				}}></div>
+				}}>
+				<div>
+					<p className="title is-4">Recents</p>
+					<p className="subtitle">
+						Nothing in recent!
+						<Link to="/add" className="ml-2">
+							Add New Item
+						</Link>
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
